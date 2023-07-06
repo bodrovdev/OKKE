ac8906314a7e6f4de529df09af6bb1bfe958f472
Igor Bodrov <bodrovigor02@gmail.com>
2023-07-06 17:56:05 +0300

    feat: 🎸 Разметка, стилизация

--- src/js/index.js
+++ src/js/index.js
@@ -20,47 +20,47 @@ burger.addEventListener('click', () => {
 })
 
 // ? --- Модалка с формой
-window.addEventListener('load', () => {
-  if (!(document.querySelector('#modal_with_form') === null)) {
+// window.addEventListener('load', () => {
+//   if (!(document.querySelector('#modal_with_form') === null)) {
 
-    let modal_with_form = document.getElementById('modal-with-form');
-    let modal_with_form_close = document.getElementById('modal-with-form-close');
-    let modal_with_form_buttons = document.querySelectorAll('.page-button');
-    let modal_with_form_formset = document.getElementById('modal_with_form_formset');
+//     let modal_with_form = document.getElementById('modal-with-form');
+//     let modal_with_form_close = document.getElementById('modal-with-form-close');
+//     let modal_with_form_buttons = document.querySelectorAll('.page-button');
+//     let modal_with_form_formset = document.getElementById('modal_with_form_formset');
 
-    function closeFormModal() {
-      modal_with_form.classList.remove('modal-form--active');
-      unlock(modal_with_form);
-    }
+//     function closeFormModal() {
+//       modal_with_form.classList.remove('modal-form--active');
+//       unlock(modal_with_form);
+//     }
 
-    // ? - Открытие модалки на нажатие кнопки
-    modal_with_form_buttons.forEach((button) => {
-      button.addEventListener('click', () => {
-        modal_with_form.classList.add('modal-form--active');
-        lock(modal_with_form);
-      })
-    })
+// ? - Открытие модалки на нажатие кнопки
+// modal_with_form_buttons.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     modal_with_form.classList.add('modal-form--active');
+//     lock(modal_with_form);
+//   })
+// })
 
-    // ? - Варианты закрытия модалки
-    window.addEventListener('click', (e) => {
-      switch (e) {
-        case (e.target === modal_with_form_close):
-          closeFormModal();
-          break;
-        case (e.target !== e.currentTarget):
-          closeFormModal();
-          break;
-      }
-    })
+// ? - Варианты закрытия модалки
+// window.addEventListener('click', (e) => {
+//   switch (e) {
+//     case (e.target === modal_with_form_close):
+//       closeFormModal();
+//       break;
+//     case (e.target !== e.currentTarget):
+//       closeFormModal();
+//       break;
+//   }
+// })
 
-    // ? - Подтверждение отправки модалки
-    modal_with_form_formset.addEventListener('submit', (e) => {
-      e.preventDefault();
-      document.querySelector('.modal-form__inner').classList.add('modal-form__inner--hidden');
-      document.querySelector('.modal-form__success').classList.add('modal-form__success--active');
-    })
-  }
-})
+// ? - Подтверждение отправки модалки
+//     modal_with_form_formset.addEventListener('submit', (e) => {
+//       e.preventDefault();
+//       document.querySelector('.modal-form__inner').classList.add('modal-form__inner--hidden');
+//       document.querySelector('.modal-form__success').classList.add('modal-form__success--active');
+//     })
+//   }
+// })
 
 // ? --- Отключение подсветки ошибок в инпутах
 window.addEventListener('load', () => {
@@ -74,13 +74,32 @@ window.addEventListener('load', () => {
 // ? --- Анимация фона элементов на главной странице
 window.addEventListener('load', () => {
   if (!(document.querySelectorAll('.index-content__item') === null)) {
-    document.querySelectorAll('.index-content__item').forEach((item) => {
-      item.addEventListener('mouseover', () => {
-        item.classList.remove('index-content__item--bg-hidden');
+
+    window.innerWidth >= 1024 ?
+      (function () {
+        document.querySelectorAll('.index-content__item').forEach((item) => {
+          item.addEventListener('mouseover', () => {
+            item.classList.remove('index-content__item--bg-hidden');
+          })
+          item.addEventListener('mouseleave', () => {
+            item.classList.add('index-content__item--bg-hidden');
+          })
+        })
+      }()) :
+
+      Array.from(document.querySelectorAll('.index-content__item')).map((item, index) => {
+        setTimeout(() => {
+          item.classList.remove('index-content__item--bg-hidden');
+        }, index * 500);
       })
-      item.addEventListener('mouseleave', () => {
-        item.classList.add('index-content__item--bg-hidden');
-      })
-    })
   }
 });
+
+// ? --- Скругление текста вокруг ссылки
+window.addEventListener('load', () => {
+
+  if (!(document.querySelectorAll('.round-link') === null)) {
+    let round_link = document.querySelector('.heading__round-link');
+    new CircleType(round_link.querySelector('.heading__round-text'));
+  }
+})
